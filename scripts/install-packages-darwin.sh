bd76462b0b22973943d4cd5eb47c6c4453b49c5f
Seferino Fernandez <seferinofz@gmail.com>
2025-05-02 23:31:39 -0700

    fix(debian,darwin): Added rust

--- scripts/install-packages-darwin.sh
+++ scripts/install-packages-darwin.sh
@@ -25,3 +25,11 @@ brew install zsh \
     fnm \
     chezmoi \
     uv
+
+if [[ $(command -v cargo) == "" ]]; then
+    echo "Installing Rust"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+else
+    echo "Updating Rust"
+    rustup update
+fi
--- scripts/install-packages-linux-debian.sh
+++ scripts/install-packages-linux-debian.sh
@@ -29,3 +29,11 @@ brew install zsh \
     uv
 
 sudo chsh -s "$(which zsh)"
+
+if [[ $(command -v cargo) == "" ]]; then
+    echo "Installing Rust"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+else
+    echo "Updating Rust"
+    rustup update
+fi
