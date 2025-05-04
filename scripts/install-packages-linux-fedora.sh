2c6c70795b92af9b7a196982fbf17e5e100d559d
Seferino Fernandez <seferinofz@gmail.com>
2025-05-03 18:55:28 -0700

    feat(fedora): Updated Fedora to use Aurora

--- scripts/install-packages-linux-fedora.sh
+++ scripts/install-packages-linux-fedora.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-sudo dnf -y update
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Updating Homebrew"
+    brew update
+fi
+
+brew install --cask font-jetbrains-mono-nerd-font
+
+brew install zsh \
+    curl \
+    git \
+    neovim \
+    eza \
+    bat \
+    ripgrep \
+    fd \
+    zoxide \
+    fzf \
+    sheldon \
+    starship \
+    chezmoi
