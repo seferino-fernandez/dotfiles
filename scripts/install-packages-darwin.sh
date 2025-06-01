9763c889971bb218da6613a67946cf04077212f8
Seferino Fernandez <seferinofz@gmail.com>
2025-05-31 21:10:27 -0700

    feat(macOS): Install lazygit

--- scripts/install-packages-darwin.sh
+++ scripts/install-packages-darwin.sh
@@ -1,37 +1,38 @@
 #!/usr/bin/env bash
 
 if [[ $(command -v brew) == "" ]]; then
-    echo "Installing Hombrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "Installing Hombrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-    echo "Updating Homebrew"
-    brew update
+  echo "Updating Homebrew"
+  brew update
 fi
 
-brew install --cask font-jetbrains-mono-nerd-font
-
 brew install zsh \
-    curl \
-    git \
-    neovim \
-    eza \
-    bat \
-    ripgrep \
-    fd \
-    zoxide \
-    fzf \
-    sheldon \
-    starship \
-    fnm \
-    chezmoi \
-    uv \
-    font-fira-code-nerd-font \
-    font-jetbrains-mono-nerd-font
+  curl \
+  git \
+  neovim \
+  eza \
+  bat \
+  ripgrep \
+  fd \
+  zoxide \
+  fzf \
+  sheldon \
+  starship \
+  fnm \
+  chezmoi \
+  uv \
+  font-fira-code-nerd-font \
+  font-jetbrains-mono-nerd-font
+
+brew install jesseduffield/lazygit/lazygit
+brew install lazygit
 
 if [[ $(command -v cargo) == "" ]]; then
-    echo "Installing Rust"
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  echo "Installing Rust"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 else
-    echo "Updating Rust"
-    rustup update
+  echo "Updating Rust"
+  rustup update
 fi
