36d0d4f316d4463527490c18cf4c6f2fcb533ff5
Seferino Fernandez <seferinofz@gmail.com>
2026-02-16 09:06:40 -0700

    feat(macos): Comment out macos core commands

--- dot_zshrc.d/core.zsh
+++ dot_zshrc.d/core.zsh
@@ -1,17 +1,18 @@
 # zoxide
-alias cd='z'
+# alias cd='z'
 
 # bat
-alias cat='bat'
+# alias cat='bat'
 
 # Neovim
 alias vim='nvim'
 alias vi='nvim'
 
 # fd
-alias find='fd'
+# alias find='fd'
 
 # Eza
-alias ls='eza --all --long --group --group-directories-first --icons --header --time-style long-iso'
-
-alias grep='rg'
+alias ls='ls -alG'
+# alias ls='eza --all --long --group --group-directories-first --icons --header --time-style long-iso'
+alias eza='eza --all --long --group --group-directories-first --icons --header --time-style long-iso'
+# alias grep='rg'
