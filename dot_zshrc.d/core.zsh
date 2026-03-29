3585c1a64fb43deeb402e7e8a3bb5b45fa01b93d
Seferino Fernandez <seferinofz@gmail.com>
2026-03-29 15:32:57 -0700

    feat: Updated Ubuntu setup and starship prompt

--- dot_zshrc.d/core.zsh
+++ dot_zshrc.d/core.zsh
@@ -2,7 +2,7 @@
 # alias cd='z'
 
 # bat
-# alias cat='bat'
+alias cat='bat'
 
 # Neovim
 alias vim='nvim'
@@ -12,7 +12,8 @@ alias vi='nvim'
 # alias find='fd'
 
 # Eza
-alias ls='ls -alG'
-# alias ls='eza --all --long --group --group-directories-first --icons --header --time-style long-iso'
+#alias ls='ls -alG'
+alias ls='eza --all --long --group --group-directories-first --icons --header --time-style long-iso'
 alias eza='eza --all --long --group --group-directories-first --icons --header --time-style long-iso'
+alias tree='eza -T --long --group --group-directories-first --icons --header --time-style long-iso'
 # alias grep='rg'
